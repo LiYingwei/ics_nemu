d01e7cc0da0ab4ff0f897ff932c76328d3c82e67
tracer-ics2015 <tracer@njuics.org>
2015-11-18 15:58:10 +0800

    >  run 14307130092 lyw Linux lyw-MacBookPro 3.16.0-54-generic #73~14.04.1-Ubuntu SMP Tue Nov 10 17:46:26 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  15:58:10 up  6:32,  3 users,  load average: 0.03, 0.24, 0.27 782b4e6437249dfee62cb88590c1c955fe3a3765

--- testcase/src/digui.c
+++ testcase/src/digui.c
@@ -6,63 +6,83 @@
 
 
 int fun20() {
+	set_bp();
 	return 13;
 }
 int fun19() {
+	set_bp();
 	return fun20();
 }
 int fun18() {
+	set_bp();
 	return fun19();
 }
 int fun17() {
+	set_bp();
 	return fun18();
 }
 int fun16() {
+	set_bp();
 	return fun17();
 }
 int fun15() {
+	set_bp();
 	return fun16();
 }
 int fun14() {
+	set_bp();
 	return fun15();
 }
 int fun13() {
+	set_bp();
 	return fun14();
 }
 int fun12() {
+	set_bp();
 	return fun13();
 }
 int fun11() {
+	set_bp();
 	return fun12();
 }
 int fun10() {
+	set_bp();
 	return fun11();
 }
 int fun9() {
+	set_bp();
 	return fun10();
 }
 int fun8() {
+	set_bp();
 	return fun9();
 }
 int fun7() {
+	set_bp();
 	return fun8();
 }
 int fun6() {
+	set_bp();
 	return fun7();
 }
 int fun5() {
+	set_bp();
 	return fun6();
 }
 int fun4() {
+	set_bp();
 	return fun5();
 }
 int fun3() {
+	set_bp();
 	return fun4();
 }
 int fun2() {
+	set_bp();
 	return fun3();
 }
 int fun1() {
+	set_bp();
 	return fun2();
 }
 
