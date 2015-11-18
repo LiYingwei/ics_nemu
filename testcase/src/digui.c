3c1e73590952770480334b42039f2e49d95b91b2
tracer-ics2015 <tracer@njuics.org>
2015-11-18 15:46:46 +0800

    >  run 14307130092 lyw Linux lyw-MacBookPro 3.16.0-54-generic #73~14.04.1-Ubuntu SMP Tue Nov 10 17:46:26 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  15:46:46 up  6:21,  3 users,  load average: 0.16, 0.26, 0.25 5de45bb16dbcacabb408009989a22f5d8f23951

---
+++ testcase/src/digui.c
@@ -0,0 +1,73 @@
+#include "trap.h"
+
+
+
+
+
+
+int fun20() {
+	return 0;
+}
+int fun19() {
+	return fun20();
+}
+int fun18() {
+	return fun19();
+}
+int fun17() {
+	return fun18();
+}
+int fun16() {
+	return fun17();
+}
+int fun15() {
+	return fun16();
+}
+int fun14() {
+	return fun15();
+}
+int fun13() {
+	return fun14();
+}
+int fun12() {
+	return fun13();
+}
+int fun11() {
+	return fun12();
+}
+int fun10() {
+	return fun11();
+}
+int fun9() {
+	return fun10();
+}
+int fun8() {
+	return fun9();
+}
+int fun7() {
+	return fun8();
+}
+int fun6() {
+	return fun7();
+}
+int fun5() {
+	return fun6();
+}
+int fun4() {
+	return fun5();
+}
+int fun3() {
+	return fun4();
+}
+int fun2() {
+	return fun3();
+}
+int fun1() {
+	return fun2();
+}
+
+int main() {
+	HIT_GOOD_TRAP;
+	return 0;
+}
+
