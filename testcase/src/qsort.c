389c18ab15949f13753541ece6589ffe3af22efd
tracer-ics2015 <tracer@njuics.org>
2015-11-18 22:34:28 +0800

    >  compile NEMU 14307130092 lyw Linux lyw-MacBookPro 3.16.0-54-generic #73~14.04.1-Ubuntu SMP Tue Nov 10 17:46:26 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  22:34:28 up 13:09,  3 users,  load average: 0.77, 0.56, 0.40 a7b08adbf817f0e4716bd3fbec40fd7f245d2193

--- testcase/src/qsort.c
+++ testcase/src/qsort.c
@@ -1,6 +1,7 @@
 #include "trap.h"
 #include<stdlib.h>
-int a[100000];
+#define N 100
+int a[N];
 int cmp(const void *a, const void *b)
 {
 	return (*(int *)a - *(int *)b);
@@ -8,9 +9,9 @@ int cmp(const void *a, const void *b)
 int main()
 {
 	int i=0;
-	for(i=0;i<100000;i++) a[i] = 100000-i;
-	qsort(a,100000,sizeof(a[0]),cmp);
-	for(i=0;i<100000;i++) nemu_assert(a[i]==i+1);
+	for(i=0;i<N;i++) a[i] = N-i;
+	qsort(a,N,sizeof(a[0]),cmp);
+	for(i=0;i<N;i++) nemu_assert(a[i]==i+1);
 	HIT_GOOD_TRAP;
 	return 0;
 }
