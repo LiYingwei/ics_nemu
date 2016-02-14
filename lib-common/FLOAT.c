5eb027e8ebc158e2528a61c511a34fd7fb096b1f
tracer-ics2015 <tracer@njuics.org>
2016-02-15 01:15:30 +0800

    >  compile NEMU 14307130092 lyw Linux lyw-MacBookPro 3.16.0-55-generic #74~14.04.1-Ubuntu SMP Tue Nov 17 10:15:59 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  01:15:30 up 1 day,  2:45,  3 users,  load average: 0.64, 0.49, 0.43 856bfde0ba3556ac910eaca89b078c8420c4f13a

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -1,40 +1,31 @@
 #include "FLOAT.h"
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-	return ((long long)a * b) >> 16;
+    long long tmp = (long long)a * b;
+	return (int)(tmp >> 16);
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-	int sign = (a < 0 ? -1 : 1) * (b < 0 ? -1 : 1);
-	if(a < 0) a = -a;
-	if(b < 0) b = -b;
-	int ret = a / b;
-	unsigned r = a % b;
-	int i = 0;
-	for(i = 0; i < 16; i++)
-	{
-		ret *= 2;
-		r *= 2;
-		while(r >= b) ret++, r-=b;
-	}
-	return ret * sign;
+    if (!(b & 0x7fffffff)) for (;;);
+    int fh = (a < 0 ? - 1 : 1) * (b < 0 ? - 1 : 1);
+    if (a < 0) a *= - 1;
+    if (b < 0) b *= - 1;
+    int ans = a / b, i = 16;
+    a %= b;
+    unsigned aa = a;
+    for (; i -- ; ){
+        ans *= 2;
+        aa *= 2;
+        if (aa >= b){
+            ans ++ ;
+            aa -= b;
+        }
+    }
+	return ans * fh;
 }
 
 FLOAT f2F(float a) {
-	/*unsigned *A = (unsigned*)&a;
-	unsigned sign = ((*A) & 0x80000000) ? -1 : 1;
-	unsigned expo = ((*A) & 0x7F800000) >> 23;
-	unsigned ret = (*A) & 0x7FFFFF;
-	if(expo == 0) return (*A) & 0x80000000;
-	else
-	{
-		ret += 0x800000;
-		//ret <<= -23 + (expo - 127) + 16; (e.i. ret <<= expo - 134;
-		if(expo >= 134) ret <<= expo - 134;
-		else ret >>= 134 - expo;
-		return (ret & 0x7FFFFFFF) * sign;
-	}*/
-	int ans = 0, mul = 0;
+    int ans = 0, mul = 0;
     union{
         float x;
         unsigned y;
@@ -50,7 +41,7 @@ FLOAT f2F(float a) {
 }
 
 FLOAT Fabs(FLOAT a) {
-	return a & 0x7FFFFFFF;
+    return a > 0 ? a : - a;
 }
 
 FLOAT sqrt(FLOAT x) {
