d82b3a051fa4c3ad85446f7dfad3b1291ed79b80
tracer-ics2015 <tracer@njuics.org>
2016-02-15 10:34:46 +0800

    >  compile NEMU 14307130092 lyw Linux lyw-MacBookPro 3.16.0-55-generic #74~14.04.1-Ubuntu SMP Tue Nov 17 10:15:59 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  10:34:46 up 1 day, 12:04,  3 users,  load average: 0.58, 0.49, 0.49 98e8cae8bf81986d49f8c31e4d562baa29cdb1ef

--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -17,6 +17,7 @@ static void do_execute() {
         DONT_TOUCH_MY_EIP = true;
         cpu.eip = op_src->val;
     }
+    else Log("====================================\n");
     //else panic("Has not impleament yet TAT\n");
     print_asm_template1();
 }
