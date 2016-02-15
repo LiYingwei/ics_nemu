a68161ea4d44625a8e864c0b1eb29c8273abe353
tracer-ics2015 <tracer@njuics.org>
2016-02-15 12:43:16 +0800

    >  compile NEMU 14307130092 lyw Linux lyw-MacBookPro 3.16.0-55-generic #74~14.04.1-Ubuntu SMP Tue Nov 17 10:15:59 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  12:43:16 up 1 day, 14:13,  4 users,  load average: 0.28, 0.27, 0.34 39d985fdc98634e533f29f5bab5e2210d5a8ad44

--- testcase/src/hello-str.c
+++ testcase/src/hello-str.c
@@ -14,11 +14,11 @@ void *sbrk(int incr) {
 }
 
 int main() {
-	/*sprintf(buf, "%s", "Hello world!\n");
-	nemu_assert(strcmp(buf, "Hello world!\n") == 0);*/
+	sprintf(buf, "%s", "Hello world!\n");
+	nemu_assert(strcmp(buf, "Hello world!\n") == 0);
 
-	sprintf(buf, "%d +\n", 55);
-	nemu_assert(strcmp(buf, "1 +\n") == 0);
+	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
+	nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
 
 	sprintf(buf, "%d + %d = %d\n", 2, 10, 12);
 	nemu_assert(strcmp(buf, "2 + 10 = 12\n") == 0);
