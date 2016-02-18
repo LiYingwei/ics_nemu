fa20dbe3708cec72e1aebe3cfcafd770bd66f39d
tracer-ics2015 <tracer@njuics.org>
2016-02-18 16:54:27 +0800

    >  compile NEMU 14307130092 lyw Linux lyw-MacBookPro 3.16.0-55-generic #74~14.04.1-Ubuntu SMP Tue Nov 17 10:15:59 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  16:54:27 up 1 day, 21:03,  3 users,  load average: 1.57, 1.38, 1.29 39c1c9c8522efa7babbbd07e63f5f6ecd0f4ddfd

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -31,8 +31,8 @@ uint32_t cache_read(hwaddr_t addr, size_t len) {
     uint32_t tag = addr >> (BLOCK_WIDTH + WAY_WIDTH);
     uint32_t index = (addr >> BLOCK_WIDTH) & SET_MASK;
     uint32_t offset = addr & BLOCK_MASK;
-    Log("cache read %x, len = %u\n", addr, (unsigned)len);
-    Log("tag: %u\tindex: %u\toffset: %x\n", tag, index, offset);
+    Log("cache read %x, len = %u", addr, (unsigned)len);
+    Log("tag: %u\tindex: %u\toffset: %x", tag, index, offset);
     for (i = 0; i < WAY_NUM; i++) {
         if (cache.set[index].block[i].valid &&
             cache.set[index].block[i].tag == tag) {
@@ -48,7 +48,7 @@ uint32_t cache_read(hwaddr_t addr, size_t len) {
     }
 
     if (hit_index[0] == -1) {
-        Log("miss");
+        Log("miss=========================================");
         hit_index[0] = get_block(index);
         for (i = 0; i < BLOCK_SIZE; i++)
             cache.set[index].block[hit_index[0]].data[i] =
