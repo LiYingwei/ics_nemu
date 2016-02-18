cd7d6510793f8ac6fe1fff855faddf89624a700f
tracer-ics2015 <tracer@njuics.org>
2016-02-18 20:29:02 +0800

    >  compile NEMU 14307130092 lyw Linux lyw-MacBookPro 3.16.0-55-generic #74~14.04.1-Ubuntu SMP Tue Nov 17 10:15:59 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  20:29:02 up 2 days, 38 min,  3 users,  load average: 1.27, 1.18, 1.20 a0dc441217ed957915b45c0c64074b7d4f3ac412

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -31,8 +31,8 @@ uint32_t cache_read(hwaddr_t addr, size_t len) {
     uint32_t tag = (addr >> (BLOCK_WIDTH + SET_WIDTH)) & TAG_MASK;
     uint32_t index = (addr >> BLOCK_WIDTH) & SET_MASK;
     uint32_t offset = addr & BLOCK_MASK;
-    //Log("cache read %x, len = %u", addr, (unsigned)len);
-    //Log("tag: %u\tindex: %u\toffset: %x", tag, index, offset);
+    Log("cache read %x, len = %u", addr, (unsigned)len);
+    Log("tag: %u\tindex: %u\toffset: %x", tag, index, offset);
     for (i = 0; i < WAY_NUM; i++) {
         if (cache.set[index].block[i].valid &&
             cache.set[index].block[i].tag == tag) {
@@ -58,7 +58,7 @@ uint32_t cache_read(hwaddr_t addr, size_t len) {
         cache.set[index].block[hit_index[0]].tag = tag;
         //Log("miss, blockid = %u==============================", hit_index[0]);
     }
-    if (offset + len >= BLOCK_SIZE && (1 || hit_index[1] == -1)) {
+    if (offset + len > BLOCK_SIZE && (1 || hit_index[1] == -1)) {
         hit_index[1] = get_block((index + 1) % SET_NUM);
         for (i = 0; i < BLOCK_SIZE; i++)
             cache.set[(index + 1) % SET_NUM].block[hit_index[1]].data[i] =
@@ -73,7 +73,7 @@ uint32_t cache_read(hwaddr_t addr, size_t len) {
     memcpy(temp, cache.set[index].block[hit_index[0]].data, BLOCK_SIZE);
     /*for(i = 0; i < BLOCK_SIZE; i++)printf("%02x", cache.set[index].block[hit_index[0]].data[i]);
     printf("\n");*/
-    if(offset + len >= BLOCK_SIZE) {
+    if(offset + len > BLOCK_SIZE) {
         memcpy(temp + BLOCK_SIZE, cache.set[index].block[hit_index[1]].data, BLOCK_SIZE);
     }
     return unalign_rw(temp + offset, 4);
