f928c9783d31a35bc50aa5ab04fee52e9095e2c6
tracer-ics2015 <tracer@njuics.org>
2016-02-22 22:54:57 +0800

    >  compile kernel 14307130092 lyw Linux lyw-MacBookPro 3.16.0-55-generic #74~14.04.1-Ubuntu SMP Tue Nov 17 10:15:59 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  22:54:57 up  1:40,  3 users,  load average: 3.20, 2.93, 2.21 34eb436eb41bc44e864d882e8d2319f70b507f6

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -45,23 +45,23 @@ uint32_t loader() {
         ph = (void *) (buf + elf->e_phoff + i * elf->e_phentsize);
 		if(ph->p_type == PT_LOAD) {
 			set_bp();
-			uint32_t p_hwaddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
+			//uint32_t p_hwaddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
             //ramdisk_write((uint8_t *) (ELF_OFFSET_IN_DISK + ph->p_offset), ph->p_vaddr, ph->p_filesz);
-            ramdisk_write((uint8_t *) (ELF_OFFSET_IN_DISK + ph->p_offset), p_hwaddr, ph->p_filesz);
+            //ramdisk_write((uint8_t *) (ELF_OFFSET_IN_DISK + ph->p_offset), p_hwaddr, ph->p_filesz);
 			/* TODO: zero the memory region
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			memset((void *) (p_hwaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
+			//memset((void *) (p_hwaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
             //memset((void *) (ph->p_vaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
-			extern uint32_t brk;
-			uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
-			if(brk < new_brk) { brk = new_brk; }
+			//extern uint32_t brk;
+			//uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
+			//if(brk < new_brk) { brk = new_brk; }
 #endif
 		}
 	}
