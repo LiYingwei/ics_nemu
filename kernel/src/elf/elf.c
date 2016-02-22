a9296b2a255491f79efd45709bf771473929b81b
tracer-ics2015 <tracer@njuics.org>
2016-02-22 22:55:50 +0800

    >  compile kernel 14307130092 lyw Linux lyw-MacBookPro 3.16.0-55-generic #74~14.04.1-Ubuntu SMP Tue Nov 17 10:15:59 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  22:55:50 up  1:41,  3 users,  load average: 3.11, 2.94, 2.25 31aa95831d799fb3c98c56a4bf179d56af675e62

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -44,7 +44,7 @@ uint32_t loader() {
 		/* Scan the program header table, load each segment into memory */
         ph = (void *) (buf + elf->e_phoff + i * elf->e_phentsize);
 		if(ph->p_type == PT_LOAD) {
-			set_bp();
+			
 			//uint32_t p_hwaddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
