88f685170465d7855e18d88d6b6fa630e04daa7f
tracer-ics2015 <tracer@njuics.org>
2016-03-01 19:17:06 +0800

    >  compile kernel 14307130092 lyw Linux lyw-MacBookPro 3.16.0-62-generic #82~14.04.1-Ubuntu SMP Fri Feb 19 18:56:33 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  19:17:06 up  5:46,  3 users,  load average: 0.31, 0.20, 0.15 50ddbcada8a838850d9693d7dcb9a515d75690

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -48,11 +48,11 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-            ramdisk_read((void *)ph->p_vaddr, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
+            //ramdisk_read((void *)ph->p_vaddr, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
 			/* TODO: zero the memory region
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			memset((void *) (ph->p_vaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
+			//memset((void *) (ph->p_vaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
