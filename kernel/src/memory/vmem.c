cb73667e5eaa1b21981350da52fdd81fde0af2cb
tracer-ics2015 <tracer@njuics.org>
2016-03-03 13:21:46 +0800

    >  compile kernel 14307130092 lyw Linux lyw-MacBookPro 3.16.0-62-generic #82~14.04.1-Ubuntu SMP Fri Feb 19 18:56:33 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  13:21:46 up 1 day, 16:01,  2 users,  load average: 0.39, 0.23, 0.26 48313841735cd36964915e4c1cec0577e967e430

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -21,7 +21,7 @@ void create_video_mapping() {
     int i;
     for(i = 0; i < (SCR_SIZE / PAGE_SIZE) + (SCR_SIZE % PAGE_SIZE != 0); i++)
     {
-        updir->val = make_pde(&video[i] - KOFFSET);
+        updir->val = make_pde(va_to_pa(&video[i]));
         video[i].val = make_pte(VMEM_ADDR + i * PAGE_SIZE);
         updir++;
     }
