bb40807f15ab6df7e66d1716f2695dcaa36ea8dd
tracer-ics2015 <tracer@njuics.org>
2016-03-03 13:05:09 +0800

    >  compile NEMU 14307130092 lyw Linux lyw-MacBookPro 3.16.0-62-generic #82~14.04.1-Ubuntu SMP Fri Feb 19 18:56:33 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  13:05:09 up 1 day, 15:44,  2 users,  load average: 0.22, 0.39, 0.37 9fe9e023b08c377337b8b979680ad4171a834275

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -76,10 +76,10 @@ void init_cond() {
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
 	/* Write some test data to the video memory. */
     video_mapping_write_test();
+#endif
 
 	/* Load the program. */
 	uint32_t eip = loader();
-#endif
 
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
 	/* Read data in the video memory to check whether 
