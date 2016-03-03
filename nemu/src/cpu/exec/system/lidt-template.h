add5676ca9d43d1eb6108a3acdf49239cd3dfa33
tracer-ics2015 <tracer@njuics.org>
2016-03-03 14:35:12 +0800

    >  compile NEMU 14307130092 lyw Linux lyw-MacBookPro 3.16.0-62-generic #82~14.04.1-Ubuntu SMP Fri Feb 19 18:56:33 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  14:35:12 up 1 day, 17:14,  2 users,  load average: 0.55, 0.52, 0.56 d204df65660979198c70f51656f9ce4fbd927a0

--- nemu/src/cpu/exec/system/lidt-template.h
+++ nemu/src/cpu/exec/system/lidt-template.h
@@ -8,14 +8,14 @@
 #define instr lidt
 
 static void do_execute() {
-    //Log("%x", op_src->addr);
+    Log("lidt %x", op_src->addr);
 #if DATA_BYTE == 4
-    cpu.IDTR = ((uint64_t)swaddr_read(op_src->addr, 4, 7))
-            + ((uint64_t)swaddr_read(op_src->addr + 4, 2, 7) << 32);
+    cpu.IDTR = ((uint64_t)swaddr_read(op_src->addr, 4, 6))
+            + ((uint64_t)swaddr_read(op_src->addr + 4, 2, 6) << 32);
 #else
-    cpu.IDTR = ((uint64_t)swaddr_read(op_src->addr, 2, 7) ) //6 means nothing, because pe=0
-            + ((uint64_t)swaddr_read(op_src->addr + 2, 1, 7) << 16)
-            + ((uint64_t)swaddr_read(op_src->addr + 3, 2, 7) << 24);
+    cpu.IDTR = ((uint64_t)swaddr_read(op_src->addr, 2, 6) ) //6 means nothing, because pe=0
+            + ((uint64_t)swaddr_read(op_src->addr + 2, 1, 6) << 16)
+            + ((uint64_t)swaddr_read(op_src->addr + 3, 2, 6) << 24);
 #endif
     //print_asm("lidt 0x%012lx", (uint64_t)cpu.GDTR);
     print_asm_template1();
