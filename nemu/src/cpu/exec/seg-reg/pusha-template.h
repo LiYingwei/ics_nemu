afe4de7cdc02ab58b53ccce59a3e4d4aa393bf3c
tracer-ics2015 <tracer@njuics.org>
2016-03-04 09:13:13 +0800

    >  compile NEMU 14307130092 lyw Linux lyw-MacBookPro 3.16.0-62-generic #82~14.04.1-Ubuntu SMP Fri Feb 19 18:56:33 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  09:13:13 up 2 days, 11:52,  2 users,  load average: 0.80, 0.58, 0.70 e1e3322b1bbc0e50df01b19d1eba98565b0cc64d

--- nemu/src/cpu/exec/seg-reg/pusha-template.h
+++ nemu/src/cpu/exec/seg-reg/pusha-template.h
@@ -12,10 +12,16 @@ static void do_execute () {
 #if DATA_BYTE == 1
 assert(0);
 #endif
-
-
+    DATA_TYPE Tmp = REG(R_ESP);
+    INSTR_PUSH(REG(R_EAX));
+    INSTR_PUSH(REG(R_ECX));
+    INSTR_PUSH(REG(R_EDX));
+    INSTR_PUSH(REG(R_EBX));
+    INSTR_PUSH(Tmp);
+    INSTR_PUSH(REG(R_EBP));
+    INSTR_PUSH(REG(R_ESI));
+    INSTR_PUSH(REG(R_EDI));
     print_asm("pusha%c", DATA_BYTE == 2 ? ' ':'d');
-    assert(0);
 }
 
 make_helper(concat3(instr,_,SUFFIX))
