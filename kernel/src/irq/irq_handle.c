1e385a1c5f18992cd336282a98bc1b522717b308
tracer-ics2015 <tracer@njuics.org>
2016-03-04 14:26:38 +0800

    >  compile kernel 14307130092 lyw Linux lyw-MacBookPro 3.16.0-65-generic #85~14.04.1-Ubuntu SMP Wed Mar 2 18:05:50 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  14:26:38 up  3:19,  3 users,  load average: 0.05, 0.20, 0.22 7dcfe74ac0fe193d7e6687b0f5af191b7b70fb21

--- kernel/src/irq/irq_handle.c
+++ kernel/src/irq/irq_handle.c
@@ -37,9 +37,9 @@ void irq_handle(TrapFrame *tf) {
 
 	int irq = tf->irq;
 
-	if (irq < 0) {set_bp();
+	if (irq < 0) {
 		panic("Unhandled exception!");
-	} else if (irq == 0x80) {
+	} else if (irq == 0x80) {set_bp();
 		do_syscall(tf);
 	} else if (irq < 1000) {
 		panic("Unexpected exception #%d at eip = %x", irq, tf->eip);
