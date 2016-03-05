0835e282e39bf92efee11cf6a82cc6bf14483ee2
tracer-ics2015 <tracer@njuics.org>
2016-03-05 22:56:10 +0800

    >  compile NEMU 14307130092 lyw Linux lyw-MacBookPro 3.16.0-65-generic #85~14.04.1-Ubuntu SMP Wed Mar 2 18:05:50 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  22:56:10 up 1 day, 59 min,  3 users,  load average: 0.40, 0.34, 0.35 adbe31d46287938a35389557d94cae7c5e5e995

--- nemu/src/cpu/exec/system/out-template.h
+++ nemu/src/cpu/exec/system/out-template.h
@@ -12,7 +12,7 @@ make_helper(concat5(instr,_,i2a,_,SUFFIX))
     int len = decode_i_b(eip + 1);
     pio_write(op_src->val, DATA_BYTE, REG(R_EAX));
     print_asm(str(instr) str(SUFFIX) " %%eax, $%d", op_src->val);
-    return len +1;
+    return len + 1;
 }
 
 make_helper(concat5(instr,_,d2a,_,SUFFIX))
