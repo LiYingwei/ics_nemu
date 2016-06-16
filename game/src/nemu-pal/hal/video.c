177b9616db18a6bb24c565be37890a3fa4537907
tracer-ics2015 <tracer@njuics.org>
2016-06-16 14:31:50 +0800

    >  compile game 14307130092 lyw Linux lyw-MacBookPro 3.16.0-69-generic #89~14.04.1-Ubuntu SMP Thu Mar 17 20:51:04 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  14:31:50 up  1:20,  3 users,  load average: 0.17, 0.17, 0.21 399851d3d535a00e4be483b0c162e35ab6db7b32

--- game/src/nemu-pal/hal/video.c
+++ game/src/nemu-pal/hal/video.c
@@ -33,8 +33,8 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *scrrect,
         dstrect = &tmp2;
         dstrect -> x = 0;
         dstrect -> y = 0;
-        dstrect -> w = (uint16_t) dst->w;
-        dstrect -> h = (uint16_t) dst->h;
+        dstrect -> w = (uint16_t) -1;
+        dstrect -> h = (uint16_t) -1;
     }
 
     int w = min(scrrect->w, dstrect->w);
